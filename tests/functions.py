f062f4043a056ea197e75f31ab11eab7649a5f0e
MarkovChenITRI <markov.chen1996@itri.org.tw>
2023-12-29 01:32:47 +0800

    Web API

--- tests/functions.py
+++ tests/functions.py
@@ -8,7 +8,7 @@ print("Supported_Options: ", json.loads(res)['supported_options'])
 res = requests.post('http://localhost:5000/index').content
 print("Module Resources: ", json.loads(res))
 
-"""
+
 #上傳資料集(模擬)，dtype, task需依照help提供的選項填寫
 data = json.dumps({'user': 'Markov', 'dataset': 'HardHat', 'dtype':'Vision2D', 'task':'ObjectDetection'})
 res = json.loads(requests.post('http://localhost:5000/push', data=data, headers={'Content-Type': 'application/json'}).content)
@@ -22,7 +22,7 @@ print(res)
 data = json.dumps({'user': 'Markov', 'dataset': 'HardHat1', 'dtype':'Vision2D', 'task':'ObjectDetection'})
 res = json.loads(requests.post('http://localhost:5000/remove', data=data, headers={'Content-Type': 'application/json'}).content)
 print(res)
-"""
+
 
 #取得使用者可存取的資源清單
 data = json.dumps({'user': 'Markov'})
@@ -30,7 +30,7 @@ res = json.loads(requests.post('http://localhost:5000/info', data=data, headers=
 print('datasets:', list(res['datasets'].keys()))
 print('modules:', list(res['modules'].keys()))
 
-"""
+
 #安裝模組(以URL, tag, local_folder來安裝模組)
 data = json.dumps({'url': 'https://github.com/R300-AI/Tensorflow-YOLOv8m_det.git'})
 res = json.loads(requests.post('http://localhost:5000/install', data=data, headers={'Content-Type': 'application/json'}).content)
@@ -43,7 +43,7 @@ print(list(res.keys()))
 data = json.dumps({'local': 'Pytorch-YOLOv8m_det'})
 res = json.loads(requests.post('http://localhost:5000/install', data=data, headers={'Content-Type': 'application/json'}).content)
 print(list(res.keys()))
-"""
+
 
 #先新增再刪除模組
 data = json.dumps({'local': 'Pytorch-YOLOv8m_det'})

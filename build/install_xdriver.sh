44980197a4f1c455e6426ac9617efe7da2fe4a46
Markov <140595764+MarkovChenITRI@users.noreply.github.com>
2024-01-02 13:14:57 +0800

    Delete build/install_xdriver.sh

--- build/install_xdriver.sh
+++
@@ -1,6 +0,0 @@
-WORK_DIR=$(readlink -f .)
-echo ${WORK_DIR}
-docker rmi -f xdriver
-docker rm -f xdriver
-docker build -f ./docker/Dockerfile . -t xdriver
-docker run --name xdriver --link mongodb:mongo -p 5000:5000 -v ${WORK_DIR}/Xdriver/data:/workspace/Xdriver/data --privileged -it xdriver

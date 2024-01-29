3deea1af0a00d6d88caa9c931d45e2e0400c2125
MarkovChenITRI <markov.chen1996@itri.org.tw>
2024-01-29 17:39:34 +0800

    update permosion

--- Xdriver/engine/plugins.py
+++ Xdriver/engine/plugins.py
@@ -49,7 +49,7 @@ class Plugins(Driver):
 
     def Run(self, dataset=None):
         entrypoint, dataset_path, output, log = self.tmp_config(self.username, dataset, True)
-        subprocess.run(['./Xdriver/dos2unix.exe', entrypoint.replace(self.xdriver_dir, '.')])
+        subprocess.run(['sudo ./Xdriver/dos2unix.exe', entrypoint.replace(self.xdriver_dir, '.')])
         subprocess.run(["bash", entrypoint, '-u', self.username, '-d', dataset])
         #shutil.rmtree(dataset_path)
         
